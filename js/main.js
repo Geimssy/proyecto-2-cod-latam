4b884c81a839bd31970d6fc96f5e03d50acf2d15
Geimssy <geimssy.alejandro@gmail.com>
2023-08-13 16:48:15 -0400

    ultimo detalle de js

--- js/main.js
+++ js/main.js
@@ -46,4 +46,15 @@ function textoParaPC() {
     }
 }
 
-
+//Para cerrar el menú automáticamente
+document.addEventListener("DOMContentLoaded", function () {
+    // Close the menu when a link is clicked
+    var navLinks = document.querySelectorAll(".navbar-nav a");
+    for (var i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener("click", function () {
+            var navbarCollapse = document.getElementById("navbarSupportedContent");
+            var bootstrapNavbar = bootstrap.Collapse.getInstance(navbarCollapse);
+            bootstrapNavbar.hide(); // Close the collapse
+        });
+    }
+});
